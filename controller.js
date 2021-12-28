670f5744280fbf9fa574f6dcab6eceff35ece1ff
hendro susanto <hero.nd93@gmail.com>
2021-12-28 13:26:26 +0700

    tampil table bhn

--- controller.js
+++ controller.js
@@ -4,5 +4,19 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function (req, res) {
-    response.ok('aplikasi rest api berjalan')
+    response.ok('aplikasi rest api berjalan', res)
+};
+
+//menampilkan data 
+
+exports.tampilbhn = function (req, res) {
+    connection.query('select * from bhn', function (error, rows, fields) {
+        if (error) {
+            connection.log(error);
+        } else {
+
+            response.ok(rows, res);
+
+        }
+    });
 };
