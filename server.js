ab30c1074ef78be6f9e1a8f307bd5f0877ffcb2b
hendro susanto <hero.nd93@gmail.com>
2021-12-28 12:50:58 +0700

    setting routes di server js

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-
+//parse applikasi json
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+
+//panggil routes
+var routes = require('./routes');
+routes(app);
+
 app.listen(3000, () => {
     console.log('Server stated on port');
 });
