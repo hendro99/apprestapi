961289a41c1cdcefdd8943adb94e7a0687b946d4
hendro susanto <hero.nd93@gmail.com>
2021-12-28 16:09:53 +0700

    update package md5

---
+++ middleware/auth.js
@@ -0,0 +1,18 @@
+var connection = require('../koneksi');
+var mysql = require('mysql');
+var md5 = require('md5');
+var response = require('../res');
+var jwt = require('jsonwebtoken');
+var config = require('../config/secret');
+var ip = require('ip');
+
+
+exports.registrasi = function (req, res) {
+    var post = {
+        username: req.body.username,
+        email: req.body.email,
+        password: md5(req.body.password),
+        role: req.body.role
+
+    }
+}
